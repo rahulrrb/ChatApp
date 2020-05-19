f53ed05619ed4f0d85aeb01cde0a14c877c0c097
Rahul Ranjan <rahulrrb39@gmail.com>
2020-05-19 12:13:20 +0530

    Modified main page

--- app/src/main/java/com/rrb/chatapp/MainActivity.kt
+++ app/src/main/java/com/rrb/chatapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.rrb.chatapp
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.appcompat.app.AppCompatActivity
@@ -29,5 +30,16 @@ class MainActivity : AppCompatActivity() {
         } else {
             super.onBackPressed()
         }
+    }
+
+    fun loginBtnNavClicked(view: View){
+
+    }
+
+    fun addChannelClicked(view: View){
+
+    }
+    fun sendMessageBtnClicked(view: View){
+        
     }
 }
